6b3dace1f638a2f8b8baa08b01ab63fb6454dc44
Mike Auty <mike.auty@gmail.com>
2016-12-26 13:37:26 +0000

    Update the setup information so it installs appropriate egg information.

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup
 
 setup(
+    name = "pynfc",
+    version = "0.1",
     setup_requires = ["cffi>=1.0.0"],
     cffi_modules = ["pynfc_build.py:ffi"],
     install_requires = ["cffi>=1.0.0"],
+    author = "Mike Auty",
+    author_email = 'mike.auty@gmail.com',
+    license = 'LGPL-3',
 )
